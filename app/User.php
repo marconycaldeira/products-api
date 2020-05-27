3645d4ba1d3600fb4d0ad17004d71362139c2f3e
Marcony <marconycaldeira@gmail.com>
2020-05-27 11:33:30 -0300

    Add soft deletes to users

--- app/User.php
+++ app/User.php
@@ -2,13 +2,15 @@
 
 namespace App;
 
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
     use Notifiable;
+    use SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
@@ -36,4 +38,7 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    protected $dates = ['deleted_at'];
+
 }
